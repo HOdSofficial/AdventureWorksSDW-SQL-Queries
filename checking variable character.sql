4973acf96df160f7e9cf1af5dca74f7ac443c28c
Saheed Hassan <118116149+HOdSofficial@users.noreply.github.com>
2024-12-22 20:11:50 +0100

    Add files via upload

---
+++ checking variable character.sql
@@ -0,0 +1,15 @@
+-- checking variable character
+SELECT CONVERT (varchar(256), SERVERPROPERTY('collation'))
+
+
+SELECT *
+FROM DimProduct
+
+WHERE Color = 'sliver'
+
+
+SELECT  FrenchDescription
+
+FROM DimProduct
+
+WHERE Color = 'sliver'
